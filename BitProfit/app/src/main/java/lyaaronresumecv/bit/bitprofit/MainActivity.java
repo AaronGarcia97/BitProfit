b645a9e6dc72f686a087de5ebc40f7a9e676219b
Alex Trujillo <alextrujillo@la-MacBook-Pro-de-Alex.local>
2017-12-08 00:43:29 -0600

    Change Graphic layouts to XML and style

--- BitProfit/app/src/main/java/lyaaronresumecv/bit/bitprofit/MainActivity.java
+++ BitProfit/app/src/main/java/lyaaronresumecv/bit/bitprofit/MainActivity.java
@@ -12,58 +12,8 @@ import android.util.TypedValue;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //Very first thing it does when app starts
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Layout
-        RelativeLayout buckysLayout = new RelativeLayout(this);
-        buckysLayout.setBackgroundColor(R.color.menu_background);
-
-        //Button
-        Button redButton = new Button(this);
-        redButton.setText("Convert");
-        redButton.setBackgroundColor(R.color.menu_button);
-
-        //Username input
-        EditText username = new EditText(this);
-
-        redButton.setId(1);
-        username.setId(2);
-
-        //Creates container in order to move it where we want it
-        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-
-        //Username container
-        RelativeLayout.LayoutParams usernameDetails = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-        );
-
-        //Give rule positions to widgets
-        usernameDetails.addRule(RelativeLayout.ABOVE, redButton.getId());
-        usernameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        usernameDetails.setMargins(0,0,0,50);
-
-        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
-
-        Resources r = getResources();
-        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,200,
-                r.getDisplayMetrics()
-        );
-
-        username.setWidth(px);
-
-        //Add widget to layout(button is layout's child(addsButton, howToAddIt)
-        buckysLayout.addView(redButton, buttonDetails);
-        buckysLayout.addView(username, usernameDetails);
-
-        //Set this contents to this view
-        setContentView(buckysLayout);
-
+        setContentView(R.layout.activity_main);
     }
 }
