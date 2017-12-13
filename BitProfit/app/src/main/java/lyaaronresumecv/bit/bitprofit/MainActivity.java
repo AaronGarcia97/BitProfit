d2eaaf8fecbfd99d3170469e5472873a144adad2
Alex Trujillo <alextrujillo@la-MacBook-Pro-de-Alex.local>
2017-12-13 02:17:23 -0600

    Enlace Buttons and EditText on MainActivity, ReDesign with new colors, MEnuButton Enlaced

--- BitProfit/app/src/main/java/lyaaronresumecv/bit/bitprofit/MainActivity.java
+++ BitProfit/app/src/main/java/lyaaronresumecv/bit/bitprofit/MainActivity.java
@@ -1,19 +1,90 @@
 package lyaaronresumecv.bit.bitprofit;
 
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Button;
 import android.graphics.Color;
 import android.content.res.Resources;
 import android.util.TypedValue;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView BitcoinPrice_tv, HoraFecha_tv;
+    private Button conversion_btn;
+    private AppBarLayout mAppBarLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        if (toolbar != null) {
+            toolbar.setTitle("Hola");
+            toolbar.setTitleTextColor(getResources().getColor(R.color.secondaryDarkColor));
+            setSupportActionBar(toolbar);
+        }
+
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+
+        HoraFecha_tv = (TextView) findViewById(R.id.fecha_hora_tv);
+        BitcoinPrice_tv = (TextView) findViewById(R.id.bitcoinprice_tv);
+
+
+        conversion_btn = (Button) findViewById(R.id.conversion_btn);
+        conversion_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.conversion_btn:
+                Toast.makeText(this, "Botón ACTUALIZAR ", Toast.LENGTH_SHORT).show();
+
+            break;
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            Toast.makeText(this, "Botón REFRESH", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
